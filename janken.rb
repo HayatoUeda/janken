78e982841bcb3a2d085301060f0aefbca039e176
EC2 Default User <ec2-user@ip-172-31-43-167.us-east-2.compute.internal>
2021-06-12 09:08:16 +0000

    あっち向いてホイ追加

--- janken.rb
+++ janken.rb
@@ -1,27 +1,73 @@
-puts "じゃんけん・・・・"
-puts "[0]グー\n[1]チョキ\n[2]パー"
-
-player_hand = gets.to_i
-program_hand = rand(3)
-
-jankens = ["グー", "チョキ","パー"]
-puts "あなたの手:#{jankens[player_hand]}, 相手の手:#{jankens[program_hand]}"
-
-if player_hand == program_hand
-  puts "あいこで"
-  return true
-elsif(player_hand == 0 && program_hand == 1)||(player_hand == 1 && program_hand == 2)||(player_hand == 2 && program_hand == 0)
-  puts "あなたの勝ちです"
-  return false
-
-else
-  puts "あなたの負けです"
-  return false
+def main
+ #勝敗が決まらない限り続ける 
+ while !IsGameDecided()
+ end
 end
 
+def IsGameDecided
+ jankenResult = JankenResult()
+ if jankenResult == 2
+   puts "あいこで.."
+   return false
+ end
 
-next_game = true
+ if !IsAhoiDecided()
+   puts "またまたじゃんけん..."
+   return false
+ end
 
-while next_game
-  next_game = janken
+ if jankenResult == 1
+   puts "やったー！勝った！"
+ else
+   puts "残念..負けたー"
+ end
+ return true
 end
+ #じゃんけん 
+def JankenResult
+ puts "じゃんけん..."
+ choices = ["グー", "チョキ", "パー"]
+ ShowStartMessage(choices)
+
+ player_hand = gets.to_i
+ program_hand = rand(choices.size)
+ ShowChoiceInfo(choices[player_hand], choices[program_hand])
+
+ result = 0 #負け :0, 勝ち:1, あいこ:2
+ if player_hand == program_hand
+   result = 2
+ elsif ((player_hand == 0 && program_hand == 1) || (player_hand == 1 && program_hand == 2) || (player_hand == 2 && program_hand == 0))
+   result = 1
+ end
+ return result
+end
+ #あっち向いてホイ 
+def IsAhoiDecided
+ puts "あっち向いて〜"
+ choices = ["上", "下", "左", "右"]
+ ShowStartMessage(choices)
+
+ player_direction = gets.to_i
+ program_direction = rand(choices.size)
+ ShowChoiceInfo(choices[player_direction], choices[program_direction])
+
+ return (player_direction == program_direction)
+end
+
+def ShowStartMessage(choices)
+ numChoices = choices.size
+ for i in 0..(numChoices-1) do
+   puts i.to_s + "(" + choices[i] + ")"
+ end
+end
+
+def ShowChoiceInfo(playerChoiceString, programChoiceString)
+ puts "ホイ！"
+ separator = "------------------------"
+ puts separator
+ puts "あなた：#{playerChoiceString}"
+ puts "相手：#{programChoiceString}"
+ puts separator
+end
+
+main()
